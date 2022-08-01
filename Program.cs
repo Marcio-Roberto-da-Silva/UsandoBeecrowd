7de29c3b72cac05e2667b5be1d45520fd9d8b03f
Marcio Roberto <marcio.silva@keeggo.com>
2022-08-01 16:04:10 -0300

    Novo exercícios

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace UsandoBeecrowd {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.ReadLine();
+
+          
+
+            int x;
+            
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            x = a + b;
+
+            Console.WriteLine("X = " + x);
+            Console.ReadKey();
+
+
         }
     }
 }
+ 
+
