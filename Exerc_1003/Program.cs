84927e344519d4c6dc69a4f6266ab168c70d2c3c
Marcio Roberto <marcio.silva@keeggo.com>
2022-08-02 10:16:03 -0300

    reorganizando repositorio

---
+++ Exerc_1003/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exerc_1003 {
+ 
+   class Program {
+        static void Main(string[] args) {
+            int Soma;
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            Soma = A + B;
+
+            Console.WriteLine("SOMA = " + Soma);
+            Console.ReadKey();
+        }
+    }
+}
